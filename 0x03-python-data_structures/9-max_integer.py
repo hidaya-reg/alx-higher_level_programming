c154a9418f24d24ca2e808100639d22bb0424f61
hidaya <hidaya.regragui@gmail.com>
2023-09-11 13:34:07 -0700

    big

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,5 +2,10 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    big = max(my_list)
+
+    big = my_list[0]
+    for num in my_list:
+        if num > big:
+            big = num
+
     return big
