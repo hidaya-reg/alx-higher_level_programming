2c226ac10be81f8615744e7897d1f9fca8696c3d
hidaya <hidaya.regragui@gmail.com>
2023-09-11 12:51:42 -0700

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, col in enumerate(row):
+            print("{:d}".format(col), end=' ' if i < len(row) - 1 else '')
+        print()
