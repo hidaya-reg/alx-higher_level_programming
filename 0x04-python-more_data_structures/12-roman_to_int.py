d452d9bb9fc509c7d3bbe1e168f422da1c6044d1
hidaya <hidaya.regragui@gmail.com>
2023-09-13 23:28:58 -0700

    fix pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,12 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rn = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     prev_value = 0
 
     for ch in reversed(roman_string):
-        value = roman_numerals.get(ch, 0)
+        value = rn.get(ch, 0)
         if value >= prev_value:
             num += value
         else:
