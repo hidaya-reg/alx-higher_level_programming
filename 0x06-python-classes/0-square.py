04bba3ce7a8eadffc4fbcb44d6a41855920fa23a
hidaya <hidaya.regragui@gmail.com>
2023-09-26 15:10:43 -0700

    task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ Define an empty class """
+
+
 class Square:
     """Class Square"""
+
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square that defines a square"""
+
+
+class Square:
+    """This class defines a square
+    Attributes:
+        __size (int): the size of square
+
+    """
+
+    def __init__(self, size):
+        """Initialize a new Square instance
+
+        Args:
+            size (int): The size of the square
+
+        """
+        self.__size = size
