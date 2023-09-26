9ff71a29f75878848118fc1f16d0d407b2f3b2a9
hidaya <hidaya.regragui@gmail.com>
2023-09-25 23:31:46 -0700

    task0: fix pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     while i < x:
         try:
-            print(f'{my_list[i]}', end = '')
+            print(f'{my_list[i]}', end='')
             i = i + 1
-        except:
+        except IndexError:
             break
     print('')
     return i
