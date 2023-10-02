b5423219f77be03b7ee880c55cfa09d463aa309b
hidaya <hidaya.regragui@gmail.com>
2023-10-02 15:15:24 -0700

    task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """A class that defines a rectangle"""
 
     number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """
@@ -100,7 +101,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
 
-        rect = ["#" * self.__width for _ in range(self.__height)]
+        rect = [str(self.print_symbol) * self.__width for _ in range(self.__height)]
         return "\n".join(rect)
 
     def __repr__(self):
