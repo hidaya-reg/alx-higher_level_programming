ba79f47a82d6742b0aa67ed70a3bc974be1f479e
hidaya <hidaya.regragui@gmail.com>
2023-10-03 13:52:15 -0700

    rr

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""defines a class `LockedClass` that restricts
+the creation of instance attributes.
+"""
+
+
 class LockedClass:
+    """A class that restricts the creation of instance attributes."""
     __slots__ = ('first_name',)
