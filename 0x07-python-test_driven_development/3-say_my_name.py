1b6e0e942c033f6a8171acf18bd071e5def8753f
hidaya <hidaya.regragui@gmail.com>
2023-10-06 13:01:34 -0700

    taslk 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -20,9 +20,8 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
+    full_name = first_name + " ";
     if last_name:
-        full_name = "{} {}".format(first_name, last_name)
-    else:
-        full_name = first_name
+        full_name += last_name
 
     print("My name is {}".format(full_name))
