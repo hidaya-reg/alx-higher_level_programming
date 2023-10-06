07429fa852b47411a92ef34f97e2954ce5670c02
hidaya <hidaya.regragui@gmail.com>
2023-10-06 13:17:48 -0700

    tt

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -19,7 +19,5 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):
-        for j in range(size):
-            print("#", end="")
-        print()
+    for _ in range(size):
+         print("#" * size)
