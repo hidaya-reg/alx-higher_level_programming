dc14cd700c18d6c58c80419cbbd3601094398e7d
hidaya <hidaya.regragui@gmail.com>
2023-10-06 14:48:43 -0700

    task6

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -20,4 +20,4 @@ def print_square(size):
         raise ValueError("size must be >= 0")
 
     for _ in range(size):
-         print("#" * size)
+        print("#" * size)
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python3
 """a function that prints a text with 2 new lines
-after each of these characters: ., ? and :"""
+after each of these characters: ., ?"""
 
 
 def text_indentation(text):
-    """prints a text with 2 new lines
-    after each of these characters: ., ? and :
+    """
+    Prints a text with 2 new lines after
+    each of these characters: ., ? and :.
 
     Args:
-        text (string): text to print
+        text (string): the input text
 
     Raises:
-        TypeError: text should be string
+        TypeError: If 'text' is not a string
     """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
 
+    output = ""
+    for char in text:
+        output += char
+        if char in ('.', '?', ':'):
+            output += '\n\n'
 
+    lines = output.splitlines()
+    for line in lines:
+        print(line.strip())
