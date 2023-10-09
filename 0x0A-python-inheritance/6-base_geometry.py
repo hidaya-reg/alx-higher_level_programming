b7879df18275c3ff4bf11aff62096f986a26118e
hidaya <hidaya.regragui@gmail.com>
2023-10-09 02:58:52 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Create BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Create BaseGeometry class"""
+
+    def area(self):
+        """raises an Exception
+        Raises:
+            Exception: Not implemeted
+        """
+        raise Exception("area() is not implemented")
