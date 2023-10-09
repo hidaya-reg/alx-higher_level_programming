3dae8c1fcffaa5a515fb6baadd09962758ccab72
hidaya <hidaya.regragui@gmail.com>
2023-10-09 06:35:22 -0700

    task10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Create square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Create Square inherits from Rectangle class"""
+
+    def __init__(self, size):
+        """Instantite an square
+        Args:
+            size (int): size of square
+
+        Returns:
+            Square: New instance of square
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Claculate area of a square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """print square"""
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
