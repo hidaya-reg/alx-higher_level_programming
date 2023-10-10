b5412d361579f8862acbdfce0d4c66e5c2b8f95f
hidaya <hidaya.regragui@gmail.com>
2023-10-10 15:26:41 -0700

    task3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+"""function that returns the JSON representation
+of an object"""
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object
+
+    Args:
+        my_obj: object to be converted to JSON
+
+    Returns:
+        A JSON-formatted string representing the input object
+    """
+    return json.dumps(my_obj)
