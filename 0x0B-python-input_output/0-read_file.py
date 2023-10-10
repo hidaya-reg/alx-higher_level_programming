e80c6faf0b94a905d03a5358ba0592aa0baa1b6b
hidaya <hidaya.regragui@gmail.com>
2023-10-10 21:40:48 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""function that reads a text file (UTF8)
+and prints it to stdout"""
 
 
 def read_file(filename=""):
-	with open(filename) as f:
+	"""function that reads a text file (UTF8)
+	and prints it to stdout
+	Args:
+		filename (str): string containing name of file
+	"""
+	with open(filename, "r", encoding="UTF-8") as f:
 		for line in f:
 			print(line, end="")
