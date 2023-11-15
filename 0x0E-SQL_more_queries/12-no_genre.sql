1c42243088513dde7f678acc3dba910c23c62cb9
hidaya <hidaya.regragui@gmail.com>
2023-11-15 15:26:50 -0800

    task12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT s.title, sg.genre_id 
+FROM tv_shows s
+LEFT JOIN tv_show_genres sg
+ON sg.show_id = s.id 
+WHERE sg.show_id IS NULL
+ORDER BY s.title, sg.genre_id ASC;
