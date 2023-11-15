b9488f588c22f694707d8b2dfb6ab6660eb506be
hidaya <hidaya.regragui@gmail.com>
2023-11-15 15:39:01 -0800

    task14

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT s.title
+FROM tv_shows s
+LEFT JOIN tv_show_genres sg
+ON s.id = sg.show_id
+LEFT JOIN tv_genres g
+ON sg.genre_id = g.id
+WHERE g.name = 'Comedy'
+ORDER BY 1 ASC;
