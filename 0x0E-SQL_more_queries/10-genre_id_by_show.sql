7b7285be53087f8018af922430d34d51ea625a36
hidaya <hidaya.regragui@gmail.com>
2023-11-15 15:21:20 -0800

    task10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT s.title, sg.genre_id 
+FROM tv_shows s
+RIGHT JOIN tv_show_genres sg
+ON sg.show_id = s.id 
+ORDER BY s.title, sg.genre_id ASC;
