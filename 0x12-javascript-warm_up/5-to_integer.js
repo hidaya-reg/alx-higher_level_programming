88b1b1e6786a114f34eb2aa0e1bd57872e130ddf
hidaya <hidaya.regragui@gmail.com>
2023-12-11 10:39:52 +0100

    task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const param = parseInt(process.argv[2]);
 if (isNaN(param) || param === undefined) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', param);
+  console.log('My number:', param);
 }
