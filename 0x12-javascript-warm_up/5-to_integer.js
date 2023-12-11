e1091d43ff25e2370a51c78506299b0605037376
hidaya <hidaya.regragui@gmail.com>
2023-12-11 10:37:02 +0100

    task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
-
-const param = parseInt(argv[2]);
+const param = parseInt(process.argv[2]);
 
 if (isNaN(param)) {
   console.log('Not a number');
