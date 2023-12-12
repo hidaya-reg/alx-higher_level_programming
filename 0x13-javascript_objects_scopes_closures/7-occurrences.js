657f118587d0619fd4a7f52f9f88df382f65dec0
hidaya <hidaya.regragui@gmail.com>
2023-12-12 20:37:05 +0100

    bash

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,5 @@
-module.exports.nbOccurences = function (list, searchElement) {
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
