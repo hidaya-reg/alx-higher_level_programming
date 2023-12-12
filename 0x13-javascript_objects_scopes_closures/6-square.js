3cf52615c547b92195225f8da69d1160720385fa
hidaya <hidaya.regragui@gmail.com>
2023-12-12 20:08:30 +0100

    task6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Rectangle = require('./5-square');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
