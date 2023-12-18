946aa2d491f996e90191f892c62fb5cceb4344b8
hidaya <hidaya.regragui@gmail.com>
2023-12-18 06:36:45 -0800

    tasks

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
 
     session = Session()
 
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    states = session.query(State).filter(State.name.like('%a%')).all()
 
-    for state in states_to_delete:
+    for state in states:
         session.delete(state)
 
     session.commit()
