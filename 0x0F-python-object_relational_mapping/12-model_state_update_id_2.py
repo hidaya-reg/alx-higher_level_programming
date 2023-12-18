b987fb31ce82f4b16536879a03484aad3fe8390b
hidaya <hidaya.regragui@gmail.com>
2023-12-18 06:30:27 -0800

    tasks

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Lists all State objects with the name passed as argument"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username, password, database = sys.argv[1:4]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state_to_update = session.query(State).filter_by(id=2).first()
+
+    state_to_update.name = "NEw Mexico"
+
+    session.commit()
+
+    session.close()
