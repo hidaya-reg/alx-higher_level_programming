db94b585dea61b91301c77c091ad9511df24e237
hidaya <hidaya.regragui@gmail.com>
2023-12-18 01:54:22 -0800

    tasks

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument."""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    host = "localhost"
+    port = 3306
+    user, password, database, state_name = sys.argv[1:5]
+
+    db = MySQLdb.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            db=database)
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM `states` WHERE BINARY `name` = %s ORDER BY `id`"
+    cursor.execute(query, (state_name,))
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
