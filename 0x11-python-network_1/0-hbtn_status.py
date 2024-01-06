01f693317c8665a0d0621b2d7a3268c4ecad8167
hidaya <hidaya.regragui@gmail.com>
2024-01-06 15:03:03 -0800

    fix style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,7 @@ and displays the response body
 
 import urllib.request
 
+
 def fetch_status():
     """Fetches the status from the URL."""
     url = 'https://alx-intranet.hbtn.io/status'
@@ -18,6 +19,7 @@ def fetch_status():
         print("\t- type:", type(body))
         print("\t- content:", body)
         print("\t- utf8 content:", utf8_content)
+
 
 if __name__ == "__main__":
     fetch_status()
