ae4d09a011ee98085097b9b75ce67ed350d676e0
hidaya <hidaya.regragui@gmail.com>
2024-01-07 12:51:54 -0800

    fix tab errors

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,9 +10,9 @@ from urllib.error import HTTPError
 
 if __name__ == "__main__":
     url = sys.argv[1]
-try:
-    with urllib.request.urlopen(url) as response:
-        decoded_response = response.read().decode('utf-8')
-        print(decoded_response)
-except HTTPError as e:
-    print("Error code: {}".format(e.code))
+    try:
+        with urllib.request.urlopen(url) as response:
+            decoded_response = response.read().decode('utf-8')
+            print(decoded_response)
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
