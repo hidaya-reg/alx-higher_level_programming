6c9a8f29056158e1d6d20155c7dadecef4429ca3
hidaya <hidaya.regragui@gmail.com>
2024-01-07 09:45:29 -0800

    task1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,15 +8,8 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python 1-hbtn_header.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
         x_request_id = response.headers.get('X-Request-Id')
-        if x_request_id:
-            print(x_request_id)
-        else:
-            print("X-Request-Id not found in the response headers")
+        print(x_request_id)
