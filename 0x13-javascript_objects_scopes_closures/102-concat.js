659e6e124070555acd8ff27d2a7ecc4bfbb68d9f
hidaya <hidaya.regragui@gmail.com>
2024-08-21 11:36:33 +0100

    correct output

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs');
 
-const fileA = process.argv[1];
-const fileB = process.argv[2];
-const destination = process.argv[3];
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const destination = process.argv[4];
 
 const dataA = fs.readFileSync(fileA, 'utf8');
 const dataB = fs.readFileSync(fileB, 'utf8');
