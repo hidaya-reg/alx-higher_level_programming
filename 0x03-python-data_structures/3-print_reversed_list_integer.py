3c644ac7142f056bd2821a4fc5d8f5366d698e12
hidaya <hidaya.regragui@gmail.com>
2024-10-13 09:39:24 +0100

    task3 1

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    [print(f"{i:d}") for i in reversed(my_list)]
+    if len(my_list) > 0:
+        [print(f"{i:d}") for i in reversed(my_list)]
