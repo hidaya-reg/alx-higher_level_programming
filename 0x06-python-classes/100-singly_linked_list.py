0d61ede5e56d386f350bae8afcea623942aa1fec
hidaya <hidaya.regragui@gmail.com>
2024-10-21 11:52:53 +0100

    task7

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -7,13 +7,8 @@ class Node:
 
     def __init__(self, data, next_node=None):
         """"Instantiation with data and next node"""
-        if not isinstance(data, int):
-            raise TypeError('data must be an integer')
-        self.__data = data
-
-        if next_node is not None and not isinstance(next_node, Node):
-            raise TypeError('next_node must be a Node object')
-        self.__next_node = next_node
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
