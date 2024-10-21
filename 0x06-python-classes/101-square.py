ba0ed46e294752403a81b02f5f1fb32608771482
hidaya <hidaya.regragui@gmail.com>
2024-10-21 15:44:59 +0100

    task8 2

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -8,9 +8,16 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square
         """
-
-        self.size = size
-        self.position = position
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+        if not isinstance(position, tuple) or len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(p, int) and p >= 0 for p in position):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
 
     @property
     def size(self):
